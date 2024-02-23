9002f197e5ef3129f549f46480089d9a63cac143
adarsh-p-git <adarshp2020@gmail.com>
2024-02-23 11:55:53 +0530

    login edit

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -19,7 +19,7 @@ function Login() {
     const user = userCredential.user;
     // ...
     console.log("login success")
-    navigate('/inventory')
+    navigate('/dashboard')
     
   })
   .catch((error) => {
